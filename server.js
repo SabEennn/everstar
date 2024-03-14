60e48d458859177f07d598366edb6cb1c0d6523e
dibesh <sabinp7278@gmail.com>
2024-03-14 21:29:35 +0545

    minor

--- server.js
+++ server.js
@@ -5,7 +5,15 @@ const path = require("path");
 // Serve static files
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/bikes", (req, res) => {
+app.get("/bikes1", (req, res) => {
+  // Send the HTML file as the response
+  res.sendFile(path.join(__dirname, "public", "src", "pages", "yourbike.html"));
+});
+app.get("/bikes2", (req, res) => {
+  // Send the HTML file as the response
+  res.sendFile(path.join(__dirname, "public", "src", "pages", "yourbike.html"));
+});
+app.get("/bikes3", (req, res) => {
   // Send the HTML file as the response
   res.sendFile(path.join(__dirname, "public", "src", "pages", "yourbike.html"));
 });
@@ -14,7 +22,7 @@ app.get("/gears", (req, res) => {
   res.send("hey here are your gears");
 });
 // Start the server
-const PORT = 3000;
+const PORT = 5500;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
